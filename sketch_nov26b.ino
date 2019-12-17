9967c41ef621e52d51600d01c16d7ac5b1e2e6f1
QSG2215873492 <51730728+QSG2215873492@users.noreply.github.com>
2019-12-17 15:10:23 +0800

    Initial commit

---
+++ sketch_nov26b.ino
@@ -0,0 +1,61 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  // H的摩斯密码
+  for(i=0;i<4;i++){
+    digitalWrite(13,HIGH);
+     delay(200);
+    if(i==3){
+     digitalWrite(13,LOW);
+      delay(2000);
+    }else{
+    digitalWrite(13,LOW);
+     delay(500);
+    }
+  }
+  // E的摩斯密码
+  digitalWrite(13,HIGH);
+    delay(200);
+  digitalWrite(13,LOW);
+    delay(2000);
+  // L的摩斯密码
+  for(int j=0;j<2;j++){
+   digitalWrite(13,HIGH);
+    delay(200);
+  digitalWrite(13,LOW);
+    delay(500);
+   digitalWrite(13,HIGH);
+    delay(200);
+  digitalWrite(13,LOW);
+    delay(1000);
+   digitalWrite(13,HIGH);
+    delay(200);
+  digitalWrite(13,LOW);
+    delay(500);
+   digitalWrite(13,HIGH);
+    delay(200);
+  digitalWrite(13,LOW);
+    delay(2000);
+  }
+  // O的摩斯密码
+  for(i=0;i<3;i++){
+    digitalWrite(13,HIGH);
+    delay(200);
+    if(i==2)
+    {
+      digitalWrite(13,HIGH);
+    delay(200);
+      digitalWrite(13,LOW);
+    delay(2000);
+    }
+    else{
+  digitalWrite(13,LOW);
+    delay(1000);
+    }
+  }
+}
